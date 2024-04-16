0a54f823c083c0fd96bd611bce5b2b897d70dc82
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2024-04-16 14:27:57 +0900

    list ssh host

--- src/vein/completer.py
+++ src/vein/completer.py
@@ -6,6 +6,20 @@ from prompt_toolkit.completion import FuzzyWordCompleter
 from prompt_toolkit.history import FileHistory
 from pathlib import Path
 
+from paramiko import SSHConfig
+from pathlib import Path
+
+def list_ssh_hosts() -> list[str]:
+    ssh_config_file = Path.home() / ".ssh/config"
+    assert ssh_config_file.exists()
+    ssh_config = SSHConfig()
+    with ssh_config_file.open() as f:
+        ssh_config.parse(f)
+    hosts = [host.get("host") for host in ssh_config._config if "host" in host]
+    flatten = sum(hosts, [])
+    flatten = [host for host in flatten if "*" not in host]
+    return flatten
+
 
 # 単語の候補
 my_completer = FuzzyWordCompleter(
@@ -14,6 +28,10 @@ my_completer = FuzzyWordCompleter(
 
 class Completer:
     def __init__(self, name: str):
+        if "host" in name:
+            self.candidate = list_ssh_hosts()
+        elif "port" in name:
+            self.candidate = ["7860", "7861", "7862", "7863", "8188", "22", "10022", "80", "443"]
         self.name = name
         history_dir = Path.home()/".local/share/vein/history"
         history_dir.mkdir(parents=True, exist_ok=True)
